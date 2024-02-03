7fd1527d615de20d4dd10589ea40838c66df880b
Abdumumin24 <133331880+Abdumumin24@users.noreply.github.com>
2024-02-03 19:17:00 +0500

    salom

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,42 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState,} from 'react';
+import {useHistory} from 'react-router-dom';
+import M from 'materialize-css';
 import "../Form.css";
+import * as bootstrap from "../../node_modules/bootstrap/dist/js/bootstrap.min.js"; 
+
 export default function Login() {
     const [regName , setRegName] = useState("");
     const [regPassword , setRegPassword] = useState("");
     const [regEmail , setRegEmail] = useState("");
     const [logEmail , setLogEmail] = useState("");
     const [logPassword , setLogPassword] = useState("");
+	const history = useHistory();
 	const getPost = ()=>{
     
 		if(!regName || !regEmail || !regPassword){
 		  console.log("Barcha maydonlar to'ldirilishi zarur");
+		  var toastElement = document.getElementById('emailErrorToasts');
+			  var emailErrorToast = new bootstrap.Toast(toastElement);
+			  emailErrorToast.show();
+			return;
 		}
 		else{
 		  if(
 			!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(regEmail)){
 			  console.log("Email manzilingizni to'g'ri kiriting");
-			// M.toast({html:"Email manzilingizni to'g'ri kiriting", classes:"#d81b60 pink darken-1"});
-			return
+			  var toastElement = document.getElementById('emailErrorToast');
+			  var emailErrorToast = new bootstrap.Toast(toastElement);
+			  emailErrorToast.show();
+			return;
 		  }
 		  else{
 			console.log(regName);
 		  console.log(regEmail);
 		  console.log(regPassword);
+		  document.getElementById("container").classList.remove("right-panel-active");
 		  }
 		}
-	  }
+	  };
 	  const regPost = ()=>{
     
 		if(!logEmail || !logPassword){
@@ -35,14 +47,15 @@ export default function Login() {
 			!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(logEmail)){
 			  console.log("Email manzilingizni to'g'ri kiriting");
 			// M.toast({html:"Email manzilingizni to'g'ri kiriting", classes:"#d81b60 pink darken-1"});
-			return
+			return;
 		  }
 		  else{
 		  console.log(logEmail);
 		  console.log(logPassword);
+		 
 		  }
 		}
-	  }
+	  };
 
     useEffect(()=>{
         const signUpButton = document.getElementById('signUp');
@@ -58,6 +71,27 @@ signInButton.addEventListener('click', () => {
 });
     }, []);
   return (
+	<>
+	<div id="emailErrorToast" class="toast mt-4 position-absolute top-0 end-0" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="5000">
+  <div class="toast-header">
+    <strong class="me-auto">Error</strong>
+    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+  </div>
+  <div class="toast-body">
+	Email xato
+  </div>
+  
+</div>
+<div id="emailErrorToasts" class="toast mt-4 position-absolute top-0 end-0" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="5000">
+  <div class="toast-header">
+    <strong class="me-auto">Error</strong>
+    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+  </div>
+  <div class="toast-body">
+	Barcha qatorlarni to'diring!
+  </div>
+  
+</div>
     <div className='loyiha'>
     <div className="container" id="container">
 	<div className="form-container sign-up-container">
@@ -72,7 +106,7 @@ signInButton.addEventListener('click', () => {
 			<input type="text" placeholder="Ism" value={regName} onChange={(e)=>setRegName(e.target.value)} />
 			<input type="email" placeholder="Email" value={regEmail} onChange={(e)=>setRegEmail(e.target.value)} />
 			<input type="password" placeholder="Parol"  value={regPassword} onChange={(e)=>setRegPassword(e.target.value)}/>
-			<button onClick={()=>getPost}>Ro'yxatdan o'tish</button>
+			<button onClick={()=>getPost()}>Ro'yxatdan o'tish</button>
 		</div>
 	</div>
 	<div className="form-container sign-in-container">
@@ -105,6 +139,8 @@ signInButton.addEventListener('click', () => {
 		</div>
 	</div>
 </div>
+
 </div>
+</>
   )
 }
